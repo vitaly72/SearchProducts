0a0be2a7f1f0afadb343bbd0fad0df8b3b560e1c
vitaly72 <vitalyqwer5@gmail.com>
2021-04-06 14:30:34 +0300

    add DetailsActivity

--- app/src/main/java/com/project/searchproducts/models/ProductDetails.java
+++ app/src/main/java/com/project/searchproducts/models/ProductDetails.java
@@ -1,14 +1,17 @@
 package com.project.searchproducts.models;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class ProductDetails {
-    private String link;
-    private List<String> imageLinks;
-
-    public ProductDetails(String link, List<String> imageLinks) {
+    private final String link;
+    private final List<String> imageLinks;
+    private final HashMap<String, String> characteristic;
+    
+    public ProductDetails(String link, List<String> imageLinks, HashMap<String, String> characteristic) {
         this.link = link;
         this.imageLinks = imageLinks;
+        this.characteristic = characteristic;
     }
 
     public String getLink() {
@@ -17,5 +20,9 @@ public class ProductDetails {
 
     public List<String> getImageLinks() {
         return imageLinks;
+    }
+
+    public HashMap<String, String> getCharacteristic() {
+        return characteristic;
     }
 }
--- app/src/main/java/com/project/searchproducts/screens/DetailsActivity.java
+++ app/src/main/java/com/project/searchproducts/screens/DetailsActivity.java
@@ -1,34 +1,20 @@
 package com.project.searchproducts.screens;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.project.searchproducts.R;
-import com.project.searchproducts.adapters.SliderAdapter;
-import com.project.searchproducts.adapters.SliderItem;
 import com.project.searchproducts.adapters.ViewPagerAdapter;
 import com.project.searchproducts.databinding.ActivityProductBinding;
 import com.project.searchproducts.models.Product;
-import com.project.searchproducts.models.ProductDetails;
 import com.project.searchproducts.utils.JSONUtils;
 import com.project.searchproducts.viewmodels.ProductsViewModel;
-import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
-import com.smarteist.autoimageslider.SliderAnimations;
-import com.smarteist.autoimageslider.SliderView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class DetailsActivity extends AppCompatActivity {
-    SliderView sliderView;
-    private SliderAdapter adapter = new SliderAdapter(this);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,34 +26,23 @@ public class DetailsActivity extends AppCompatActivity {
         String movieJsonString = intent.getStringExtra("product");
         Product product = JSONUtils.getGsonParser().fromJson(movieJsonString, Product.class);
 
-        productBinding.textView.setText(product.getTitle());
+        productBinding.textViewName.setText(product.getTitle());
+        productBinding.textViewPrice.setText(product.getPrice());
 
         productsViewModel.detailsProduct(product.getDetailsLink());
         productsViewModel.getProductsDetailsData().observe(this, response -> {
             if (response != null) {
                 System.out.println("response.getImageLinks().size() = " + response.getImageLinks().size());
-
-//                if (response.getImageLinks() != null) {
-//                    for (int i = 0; i < response.getImageLinks().size(); i++) {
-//                        System.out.println("link = " + response.getImageLinks().get(i));
-//
-//                        SliderItem sliderItem = new SliderItem();
-//                        sliderItem.setImageUrl(response.getImageLinks().get(i));
-//                        sliderItem.setDescription("");
-//
-//                        adapter.addItem(sliderItem);
-//                    }
-//                }
-
                 ViewPagerAdapter adapter = new ViewPagerAdapter(this, response.getImageLinks());
                 productBinding.viewPager.setAdapter(adapter);
+
+                String characteristics = "";
+                for (String key : response.getCharacteristic().keySet()) {
+                    characteristics += key + "\t" + response.getCharacteristic().get(key) + "\n";
+                    System.out.println(key);
+                }
+                productBinding.textViewCharacteristics.setText(characteristics);
             }
         });
-//
-//        productBinding.imageSlider.setSliderAdapter(adapter);
-//        productBinding.imageSlider.setIndicatorAnimation(IndicatorAnimationType.WORM);
-//        productBinding.imageSlider.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
-//        productBinding.imageSlider.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
-//        productBinding.imageSlider.startAutoCycle();
     }
 }
